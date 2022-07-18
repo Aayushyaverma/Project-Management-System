04cc74c07e70aa3eedfd6b20fae464b5dcc3a6cd
Aastha Mahindra <69888214+aasthamahindra@users.noreply.github.com>
2022-07-18 10:48:24 +0530

    Dashboard Component

---
+++ pmsystem-react-client/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+//rcc: Creates class based components. 
+//Class based components maps our state to our component property.
+export class Dashboard extends Component {
+  render() {
+    return (
+        // jsx allows us to have HTML inside the JavaScript code.
+      <h1> Welcome to the Dashboard</h1>
+    )
+  }
+}
+
+export default Dashboard;
