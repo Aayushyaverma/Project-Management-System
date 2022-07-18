24267462554b6f34e7314ecbe931e2eddeca28b0
Aastha Mahindra <69888214+aasthamahindra@users.noreply.github.com>
2022-07-18 12:04:56 +0530

    Update Dashboard.js

--- pmsystem-react-client/src/components/Dashboard.js
+++ pmsystem-react-client/src/components/Dashboard.js
@@ -7,12 +7,24 @@ export class Dashboard extends Component {
   render() {
     return (
         // jsx allows us to have HTML inside the JavaScript code.
-        <div>
-
-            <h1> Welcome to the Dashboard</h1>
-            <ProjectItem/>
-
+        <div className="projects">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="display-4 text-center">Projects</h1>
+                    <br />
+                    <a href="ProjectForm.html" class="btn btn-lg btn-info">
+                        Create a Project
+                    </a>
+                    <br />
+                    <hr />
+                    <ProjectItem/>
+                </div>
+            </div>
         </div>
+        
+    </div>
+
     )
   }
 }
