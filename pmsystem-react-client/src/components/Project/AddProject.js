b14a778756195a7d0d3c211bd7a6d97bbf88c53e
Aastha Mahindra <69888214+aasthamahindra@users.noreply.github.com>
2022-07-23 12:25:58 +0530

    Update AddProject.js

--- pmsystem-react-client/src/components/Project/AddProject.js
+++ pmsystem-react-client/src/components/Project/AddProject.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import ProjectServices from '../../services/ProjectServices'
-import {PropTypes} from 'prop-types'
+import { PropTypes } from 'prop-types'
+import classNames from 'classnames'
 
 
 //rafce: stateless component
@@ -14,6 +15,17 @@ const AddProject = () => {
     const [description, setDescription] = useState('')
     const [start_date, setStartDate] = useState('')
     const [end_date, setEndDate] = useState('')
+
+    const initialStateErrors = {
+        projectName: '',
+        projectIdentifier: '',
+        description: ''
+    }
+
+
+    const [errors, setErrors] = useState(initialStateErrors);
+
+
     const navigate = useNavigate();
 
     const saveProject = (e) => {
@@ -26,78 +38,102 @@ const AddProject = () => {
                 console.log(response.data)
                 navigate('/dashboard');
             }).catch(error => {
-                console.log(error.response)
+                //console.log(error.response)
+                const errData = error.response.data
+                setErrors({
+                    ...error,
+                    projectName: errData.projectName,
+                    projectIdentifier: errData.projectIdentifier,
+                    description: errData.description
+                })
             })
     }
 
     return (
         <div>
             <div className='project'>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 m-auto">
-                        <h5 className="display-4 text-center">Create Project</h5>
-                        <hr />
-                        <form>
-                            <div className="form-group">
-                                <input type="text"  
-                                    className="form-control form-control-lg "
-                                    placeholder="Project Name"
-                                    name='projectName'
-                                    value={projectName}
-                                    onChange={(e) => setProjectName(e.target.value)}
-                                />
-                            </div>
-                            <br />
-                            <div className="form-group">
-                                <input type="text" 
-                                    className="form-control form-control-lg"
-                                    placeholder="Unique Project ID"
-                                    name='projectIdentifier'
-                                    value={projectIdentifier}
-                                    onChange={(e) => setProjectIdentifier(e.target.value)}
-                                />
-                            </div>
-                            <br />
-                            <div className="form-group">
-                                <textarea
-                                    className="form-control form-control-lg"
-                                    placeholder="Project Description"
-                                    name='description'
-                                    value={description}
-                                    onChange={(e) => setDescription(e.target.value)}
-                                />
-                            </div>
-                            <br />
-                            <h6>Start Date</h6>
-                            <div className="form-group">
-                                <input type="date"
-                                    className="form-control form-control-lg"
-                                    name="start_date"
-                                    value={start_date}
-                                    onChange={(e) => setStartDate(e.target.value)}
-                                />
-                            </div>
-                            <br />
-                            <h6>Estimated End Date</h6>
-                            <div className="form-group">
-                                <input type="date"
-                                    className="form-control form-control-lg"
-                                    name="end_date"
-                                    value={end_date}
-                                    onChange={(e) => setEndDate(e.target.value)}
-                                />
-                            </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h5 className="display-4 text-center">Create Project</h5>
+                            <hr />
+                            <form>
+                                <div className="form-group">
+                                    <input type="text"
+                                        className={classNames("form-control form-control-lg", {
+                                            "is-invalid": errors.projectName
+                                        })}
+                                        placeholder="Project Name"
+                                        name='projectName'
+                                        value={projectName}
+                                        onChange={(e) => setProjectName(e.target.value)}
+                                    />
+                                    {errors.projectName && (
+                                        <div className='invalid-feedback'>{errors.projectName}</div>
+                                    )}
+                                </div>
+                                <br />
 
-                            <button style={{ backgroundColor: "#3e7e80" }} className='btn btn-success btn-lg mt-3' onClick={(e) => saveProject(e)}>Submit</button>
-                        </form>
+                                <div className="form-group">
+                                    <input type="text"
+                                        className={classNames("form-control form-control-lg", {
+                                            "is-invalid": errors.projectIdentifier
+                                        })}
+                                        placeholder="Unique Project ID"
+                                        name='projectIdentifier'
+                                        value={projectIdentifier}
+                                        onChange={(e) => setProjectIdentifier(e.target.value)}
+                                    />
+                                    {errors.projectIdentifier && (
+                                        <div className='invalid-feedback'>{errors.projectIdentifier}</div>
+                                    )}
+                                </div>
+                                <br />
+
+                                <div className="form-group">
+                                    <textarea
+                                        className={classNames("form-control form-control-lg", {
+                                            "is-invalid": errors.description
+                                        })}
+                                        placeholder="Project Description"
+                                        name='description'
+                                        value={description}
+                                        onChange={(e) => setDescription(e.target.value)}
+                                    />
+                                    {errors.description && (
+                                        <div className='invalid-feedback'>{errors.description}</div>
+                                    )}
+                                </div>
+                                <br />
+
+                                <h6>Start Date</h6>
+                                <div className="form-group">
+                                    <input type="date"
+                                        className="form-control form-control-lg"
+                                        name="start_date"
+                                        value={start_date}
+                                        onChange={(e) => setStartDate(e.target.value)}
+                                    />
+                                </div>
+                                <br />
+                                <h6>Estimated End Date</h6>
+                                <div className="form-group">
+                                    <input type="date"
+                                        className="form-control form-control-lg"
+                                        name="end_date"
+                                        value={end_date}
+                                        onChange={(e) => setEndDate(e.target.value)}
+                                    />
+                                </div>
+
+                                <button style={{ backgroundColor: "#3e7e80" }} className='btn btn-success btn-lg mt-3' onClick={(e) => saveProject(e)}>Submit</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
 
 export default (AddProject);
-
